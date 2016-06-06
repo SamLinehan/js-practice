b428849e393d7594dc11e1e75737919f34a415bb
Samuel <samuel.linehan15@gmail.com>
2016-06-06 11:07:43 -0400

    ramp numbers

--- main.js
+++ main.js
@@ -12,5 +12,19 @@ function countLetters(str) {
   }
   return result;
 }
+// console.log(countLetters("hello"));
 
-console.log(countLetters("hello"));
+// Ramp Number
+
+function rampNumber(n) {
+  var numberArray = n.toString().split("").map(function(t){return parseInt(t)});
+  for(var i = 0; i < numberArray.length; i++) {
+    if(numberArray[i] > numberArray[i + 1]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(rampNumber(1234));
+console.log(rampNumber(1131));
