fb8e486552a9d2854977c5f93da241b502eb344f
Samuel <samuel.linehan15@gmail.com>
2016-06-10 07:57:12 -0400

    solved more problems

--- main.js
+++ main.js
@@ -26,5 +26,59 @@ function rampNumber(n) {
   return true;
 }
 
-console.log(rampNumber(1234));
-console.log(rampNumber(1131));
+// console.log(rampNumber(1234));
+// console.log(rampNumber(1131));
+
+// Palindrome
+
+function isPalindrome(str) {
+    var reversedString = "";
+    var splitString = str.split("");
+    for(var i = splitString.length - 1; i >= 0; i--) {
+      reversedString += splitString[i];
+    }
+    if(str === reversedString) {
+      return true;
+    } else {
+      return false;
+    }
+}
+
+// console.log(isPalindrome("racecar"));
+// console.log(isPalindrome("hello"));
+
+// Anagrams
+
+function isAnagram(str1, str2) {
+  var sorted1 = str1.split("").sort().join("");
+  var sorted2 = str2.split("").sort().join("");
+
+  console.log(sorted1);
+  console.log(sorted2);
+
+  if(sorted1 === sorted2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// console.log(isAnagram("examples", "selpmaxe"))
+
+// Up down
+
+function upDown(arr) {
+  var result = [];
+  for(var i = 0; i < arr.length; i++) {
+    if(arr[i] < arr[i + 1]) {
+      result.push("up");
+    } else if(arr[i] > arr[i + 1]) {
+      result.push("down");
+    } else if(arr[i] === arr[i + 1]) {
+      result.push("even");
+    }
+  }
+  return result;
+}
+
+console.log(upDown([6,3,5,4,3,4,4,5]));
